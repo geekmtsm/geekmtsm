0fafd5b7cfb27c56144ff04336c36cad96fddb96
nekova <nekonikovaaaan+github@gmail.com>
2013-03-23 20:23:26 +0900

    Require boot.rb

--- mtsmhack.rb
+++ mtsmhack.rb
@@ -1,12 +1,4 @@
-require 'bundler/setup'
-Bundler.require(:default)
-
-Twitter.configure do |config|
-  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-  config.oauth_token = ENV['TWITTER_ACCESS_TOKEN']
-  config.oauth_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-end
+require File.expand_path('../boot', __FILE__)
 
 res = Connpass.event_search(series_id: 206)
 exit if res.results_returned.zero?
