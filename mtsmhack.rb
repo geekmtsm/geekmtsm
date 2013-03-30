bcff064a5787182992fa27558149b6231518f351
itzki <itzki.h@gmail.com>
2013-03-30 17:05:58 +0900

    Change to tweet on Tue, Thu and Sat

--- mtsmhack.rb
+++ mtsmhack.rb
@@ -1,9 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
+now = Time.now.utc
+exit unless [2, 4, 6].include?(now.wday)
+
 res = Connpass.event_search(series_id: 206)
 exit if res.results_returned.zero?
 
-now = Time.now.utc
 res.events.reverse.each do |e|
   started_at = Time.parse(e.started_at).utc
   next if now > started_at
