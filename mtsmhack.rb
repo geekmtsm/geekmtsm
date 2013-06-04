db0b11b90f2bf3f3630fa860fdf5c3ec1f1b6bc6
itzki <itzki.h@gmail.com>
2013-06-04 15:04:23 +0900

    Limit only 2 upcoming events

--- mtsmhack.rb
+++ mtsmhack.rb
@@ -6,9 +6,8 @@ exit unless [2, 4, 6].include?(now.wday)
 res = Connpass.event_search(series_id: 206)
 exit if res.results_returned.zero?
 
-res.events.reverse.each do |e|
+res.events.reject{|e| now > Time.parse(e.started_at).utc }.reverse.take(2).each do |e|
   started_at = Time.parse(e.started_at).utc
-  next if now > started_at
   tweet = %[【定期POST】#{e.title} #{started_at.strftime('%Y/%m/%d')}開催 #{e.event_url} ##{e.hash_tag}]
   if Twitter.credentials?
     Twitter.update(tweet)
